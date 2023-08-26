bd93ab434892510a950c628e31436a4c1afacb8f
tpujari123 <tpujari@airspan.com>
2023-08-27 00:10:23 +0530

    spur_network_find_using_sp

--- VIProject/Network_Analysis/Find_dep_nodes.py
+++ VIProject/Network_Analysis/Find_dep_nodes.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 # Create a graph
 linked_list_df = pd.read_excel(r"Data\ll_test_single_net.xlsx",usecols=['source', 'target'])
-no_of_rows=linked_list_df.shape[0] #get number of rows
 linked_list_df.dropna(inplace=True)
 linked_list_df['links'] = linked_list_df['source'] + '-' + linked_list_df['target']
 linked_list_df.drop_duplicates(['links'], keep='first', inplace=True)
@@ -11,30 +10,48 @@ linked_list_df.drop_duplicates(['links'], keep='first', inplace=True)
 Graphtype = nx.Graph()
 G = nx.from_pandas_edgelist(linked_list_df, create_using=Graphtype)
 
-# Perform DFS traversal to find dependent nodes
-def dfs(graph, node, visited):
-    visited.append(node)
-    for neighbor in graph.neighbors(node):
-        if neighbor not in visited:
-            dfs(graph, neighbor, visited)
+def get_farthest_node(G: Graphtype,source: str,edge_to_remove: tuple) -> str:
+    shortest_path_length_a_b = nx.shortest_path_length(G, source=gne_node, target=edge_to_remove[0])
+    shortest_path_length_a_c = nx.shortest_path_length(G, source=gne_node, target=edge_to_remove[1])
+    if shortest_path_length_a_b > shortest_path_length_a_c:
+        start_node = edge_to_remove[0]
+    else:
+        start_node = edge_to_remove[1]
+    return start_node
 
-visited = list()
-node='a10_GNE'
-dfs(G, node,  visited)
-print("from GNE",visited)
+edge_to_remove=('a15','a13')
+gne_node='a10_GNE'
 
-edge_to_remove=('a12','a11')
+start_node=get_farthest_node(G,gne_node,edge_to_remove)
 G.remove_edge(*edge_to_remove)
-#edge_to_remove=('a5','a4')
-#G.remove_edge(*edge_to_remove)
-node1=visited.index(edge_to_remove[0])
-print("node1",node1)
-node2=visited.index(edge_to_remove[1])
-print("node1",node2)
-leftest_ele_ind=max(node1,node2)
-dependent_nodes_list = list()
-dfs(G, visited[leftest_ele_ind],  dependent_nodes_list)
-print("from removed edge")
-print("dependent nodes",dependent_nodes_list)
+dfs_tree = nx.dfs_tree(G, source=start_node)
 
+print("\nDependent Nodes:")
+print(dfs_tree.nodes())
 
+# # Perform DFS traversal to find dependent nodes
+# def dfs(graph, node, visited):
+#     visited.append(node)
+#     for neighbor in graph.neighbors(node):
+#         if neighbor not in visited:
+#             dfs(graph, neighbor, visited)
+#
+# visited = list()
+# node='a10_GNE'
+# dfs(G, node,  visited)
+# print("from GNE",visited)
+#
+# edge_to_remove=('a12','a11')
+# G.remove_edge(*edge_to_remove)
+# #edge_to_remove=('a5','a4')
+# #G.remove_edge(*edge_to_remove)
+# node1=visited.index(edge_to_remove[0])
+# print("node1",node1)
+# node2=visited.index(edge_to_remove[1])
+# print("node1",node2)
+# leftest_ele_ind=max(node1,node2)
+# dependent_nodes_list = list()
+# dfs(G, visited[leftest_ele_ind],  dependent_nodes_list)
+# print("from removed edge")
+# print("dependent nodes",dependent_nodes_list)
+#
